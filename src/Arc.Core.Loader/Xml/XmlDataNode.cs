90f73cd1410a6435461d5f25d3cbdfe92ec076dc
Hexi <sartinin.kostya@gmail.com>
2021-02-10 20:13:32 +0200

    Implemented basic XML parsing

---
+++ src/Arc.Core.Loader/Xml/XmlDataNode.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Arc.Core.Loader.Xml
+{
+    public class XmlDataNode
+    {
+        private readonly XElement _xmlElement;
+
+        public XmlDataNode(XElement xmlElement)
+        {
+            _xmlElement = xmlElement;
+        }
+
+
+        public string Name => _xmlElement.FirstAttribute.Value;
+
+
+        public XmlDataNode Resolve(string path = null)
+            => new XmlDataNode(_xmlElement.Elements().Single(n => n.FirstAttribute.Value == path));
+        
+        public XmlDataNode ResolveAll()
+            => new XmlDataNode(_xmlElement);
+        
+        public void ResolveAll(Action<XmlDataNode> context)
+             => context.Invoke(ResolveAll());
+        
+        public T? Resolve<T>(string path = null) where T : struct
+            => (T?)Convert.ChangeType(_xmlElement.Elements().Single(n => n.FirstAttribute.Value == path).LastAttribute.Value, typeof(T));
+    }
+}
--- src/Arc.Core.Templates/Field/FieldTemplate.cs
+++ src/Arc.Core.Templates/Field/FieldTemplate.cs
@@ -2,15 +2,29 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Text;
+using Arc.Core.Loader;
+using Arc.Core.Loader.Xml;
 
 namespace Arc.Core.Templates.Field
 {
     public class FieldTemplate : ITemplate
     {
-        public FieldTemplate(int id)
+        public FieldTemplate(int id, XmlDataNode dataNode)
         {
+            
             ID = id;
-            Portals = new Dictionary<int, PortalTemplate>();
+            dataNode.Resolve("info").ResolveAll(i =>
+            {
+                Limits = (FieldLimitations)(i.Resolve<int>("fieldLimit") ?? 0);
+
+
+                ReturnField = i.Resolve<int>("returnMap");
+                ForcedReturnField = i.Resolve<int>("forcedReturn");
+                if (ReturnField == 999999999) ReturnField = null;
+                if (ForcedReturnField == 999999999) ForcedReturnField = null;
+
+
+            });
         }
 
         public int ID { get; set; }
@@ -22,8 +36,8 @@ namespace Arc.Core.Templates.Field
         public string ScriptOnUserEnter { get; set; }
         public string ScriptField { get; set; }
 
-        public int ReturnField { get; set; }
-        public int ForcedReturnField { get; set; }
+        public int? ReturnField { get; set; }
+        public int? ForcedReturnField { get; set; }
 
         public float MobRate { get; set; }
         public int MobCapacity { get; set; }
